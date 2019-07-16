70279f536bb819f3061b3fba431409916ef4a0a6
KJ Wilson <yourboykjdev@gmail.com>
2019-07-16 00:54:02 -0700

    implemented vertex node and add vertices to graph :tired_face:

--- challenge_1.py
+++ challenge_1.py
@@ -1,38 +1,127 @@
 import ast
 
 class Vertex(object):
-    def __init__(self, data):
-        self.data = data
-        self.pointers = set()
-        
-    def points_to(self, pointer):
-        self.pointers.add(pointer)
 
-class Graph(object):
-    def __init__(self, FILE_PATH = ''):
-        pass
+    def __init__(self, vertex):
+        """Initialize a vertex and its neighbors.
+        neighbors: set of vertices adjacent to self,
+        stored in a dictionary with key = vertex,
+        value = weight of edge between self and neighbor.
+        """
+        self.id = vertex
+        self.neighbors = {}
 
-    def add_vertex(self, vert):
-        '''adds an instance of Vertex to the graph.'''
-        pass
-      
-    def add_edge(self, fromVert, toVert, weight = None):
-        '''If weight is None: Adds a new, directed edge to the graph that connects two vertices.
-        Else: Adds a new, weighted, directed edge to the graph that connects two vertices.'''
-        pass
-      
-    def get_vertex(self, vertKey):
-        '''finds the vertex in the graph named vertKey.'''
-        pass
-      
-    def get_vertices(self): 
-        '''returns the list of all vertices in the graph.'''
-        pass
-      
-    def get_neighbors(self, x): 
-        '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
-        pass
+    def add_neighbor(self, vertex, weight=0):
+        """Add a neighbor along a weighted edge."""
+        # check if vertex is already a neighbor
+        if vertex in self.neighbors:
+            return
+        # if not, add vertex to neighbors and assign weight.
+        self.neighbors[vertex] = weight
 
+    def __str__(self):
+        """Output the list of neighbors of this vertex."""
+        return f'{self.id} adjacent to {[x.id for x in self.neighbors]}'
+
+    def get_neighbors(self):
+        """Return the neighbors of this vertex."""
+        # return the neighbors
+        return self.neighbors
+
+    def get_id(self):
+        """Return the id of this vertex."""
+        return self.id
+
+    def get_edge_weight(self, vertex):
+        """Return the weight of this edge."""
+        # return the weight of the edge from this
+        # vertex to the given vertex.
+        try:
+            return self.neighbors[vertex]
+        except:
+            raise ValueError("no weight found")
+
+
+""" Graph Class
+A class demonstrating the essential
+facts and functionalities of graphs.
+"""
+
+
+class Graph:
+    def __init__(self):
+        """Initialize a graph object with an empty dictionary."""
+        self.vertList = {}
+        self.numVertices = 0
+
+    def add_vertex(self, key):
+        """Add a new vertex object to the graph with the given key and return the vertex."""
+        # increment the number of vertices
+        self.numVertices += 1
+        # create a new vertex
+        new_vertex = Vertex(key)
+        # add the new vertex to the vertex list
+        self.vertList[key] = new_vertex
+        # return the new vertex
+        return new_vertex
+
+    def get_vertex(self, key):
+        """Return the vertex if it exists"""
+        # return the vertex if it is in the graph
+        try:
+            return self.vertList[key]
+        except:
+            raise KeyError("no vertex found")
+
+    def add_edge(self, key1, key2, weight=0):
+        """add an edge from vertex with key `key1` to vertex with key `key2` with a weight."""
+        # if either vertex is not in the graph,
+        if 
+        # add it - or return an error (choice is up to you).
+        # TODO if both vertices in the graph, add the
+        # edge by making key1 a neighbor of key2
+        # and using the addNeighbor method of the Vertex class.
+        # Hint: the vertex key1 is stored in self.vertList[f].
+
+    def get_vertices(self):
+        """return all the vertices in the graph"""
+        return self.vertList.keys()
+
+    def __iter__(self):
+        """Iterate over the vertex objects in the graph, to use sytax: for v in g"""
+        return iter(self.vertList.values())
+
+
+# Driver code
+
+
+if __name__ == "__main__":
+
+    # Challenge 1: Create the graph
+
+    g = Graph()
+
+    # Add your friends
+    g.add_vertex("Friend 1")
+    g.add_vertex("Friend 2")
+    g.add_vertex("Friend 3")
+
+    # ...  add all 10 including you ...
+
+    # Add connections (non weighted edges for now)
+    g.add_edge("Friend 1", "Friend 2")
+    g.add_edge("Friend 2", "Friend 3")
+
+    # Challenge 1: Output the vertices & edges
+    # Print vertices
+    print("The vertices are: ", g.get_vertices(), "\n")
+
+    # Print edges
+    print("The edges are: ")
+    for v in g:
+        for w in v.get_neighbors():
+            print("( %s , %s )" % (v.getId(), w.getId()))
+            
 content = open("graph_data.txt")
 content = (list(content))
 
