ae9a6012ed2e7c7e1ffe25686b3f619c56f8ff2a
KJ Wilson <yourboykjdev@gmail.com>
2019-07-31 22:20:01 -0700

    added test input of friends :zap:

--- challenges/challenge1/graph_test.py
+++ challenges/challenge1/graph_test.py
@@ -5,31 +5,32 @@ from vertex import Vertex
 class GraphTest(unittest.TestCase):
     def test_add_vertex(self):
         graph = Graph()
-        graph.add_vertex("apple")
-        graph.add_vertex("banana")
+        graph.add_vertex("Abe")
+        graph.add_vertex("Max")
+        graph.add_vertex("Kyle")
 
-        self.assertEqual(2, graph.get_num_vertices())
-        self.assertIsInstance(graph.get_vertex("apple"), Vertex)
+        self.assertEqual(3, graph.get_num_vertices())
+        self.assertIsInstance(graph.get_vertex("Abe"), Vertex)
 
 
     def test_add_edge(self):
         graph = Graph()
-        graph.add_vertex("apple")
-        graph.add_vertex("banana")
-        graph.add_vertex("coconut")
+        graph.add_vertex("Abe")
+        graph.add_vertex("Max")
+        graph.add_vertex("Kyle")
 
-        graph.add_edge("apple", "banana")
-        graph.add_edge("apple", "coconut", 3)
+        graph.add_edge("Abe", "Max")
+        graph.add_edge("Abe", "Kyle", 2)
 
         self.assertEqual(3, graph.get_num_vertices())
         self.assertEqual(2, graph.get_num_edges())
 
-        graph.add_edge("pineapple", "strawberry")
+        graph.add_edge("Kennis", "Kaleb")
 
         self.assertEqual(5, graph.get_num_vertices())
         self.assertEqual(3, graph.get_num_edges())
         self.assertCountEqual(
-            ["apple", "banana", "coconut", "pineapple", "strawberry"],
+            ["Max", "Kyle", "Abe", "Kennis", "Kaleb"],
             graph.get_vertices())
 
 if __name__ == "__main__":
