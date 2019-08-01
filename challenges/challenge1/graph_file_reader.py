678f368263a0c06e823ba0a32625e2ef6098acfe
KJ Wilson <yourboykjdev@gmail.com>
2019-07-31 22:39:04 -0700

    added notes to graph_file_reader :pencil:

--- challenges/challenge1/graph_file_reader.py
+++ challenges/challenge1/graph_file_reader.py
@@ -6,27 +6,41 @@ def read_from_file(filename):
     lines = file.readlines()
     file.close()
 
-    # Check if it is a graph or digraph
-    graph_or_digraph_str =  lines[0].strip() if len(lines) > 0 else None
-    if graph_or_digraph_str != "G" and graph_or_digraph_str != "D":
+    # if len(lines) > 0:
+    #     lines[0].strip()
+    # else:
+    #     None
+
+    # Check if it is a graph or directed graph
+    g_or_d_str =  lines[0].strip() if len(lines) > 0 else None
+    print(lines[0].strip())
+    if g_or_d_str != "G" and g_or_d_str != "D":
         raise Exception("File must start with G or D.")
-    is_bidirectional = graph_or_digraph_str == "G"
+    is_bidirectional = g_or_d_str == "G"
 
     g = Graph()
 
     # Add all vertices
+    print(lines[1].strip())
     for vertex_key in lines[1].strip("() \n").split(","):
         g.add_vertex(vertex_key)
 
     # Add all edges
     for line in lines[2:]:
         # Split components of edge
+        print(f"line: {line}")
         new_edge = line.strip("() \n").split(",")
+        print(f"edge: {new_edge}")
         if len(new_edge) < 2 or len(new_edge) > 3:
             raise Exception("Lines adding edges must include 2 or 3 values")
         
         # Get vertices
         vertex1, vertex2 = new_edge[:2]
+
+        # if len(new_edge) == 3:
+        #     int(new_edge[2])
+        # else:
+        #     None
 
         # Get weight if it exists
         weight = int(new_edge[2]) if len(new_edge) == 3 else None
